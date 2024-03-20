041543aaa822b5d76e7b63970fed037cbf020535
charliebi <rabbitsgo1010@gmail.com>
2024-03-21 08:26:04 +1100

    V1.0 webserver

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder="routes/templates")
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "random")
 
 app.register_blueprint(router)
-
+#
 if __name__ == '__main__':
     app.run(port=os.getenv("FLASK_PORT", 5000))
 
