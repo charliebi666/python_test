7100cff48983208cc20125e82036dd3b86e63fcf
charliebi <rabbitsgo1010@gmail.com>
2024-03-23 10:43:52 +1100

    V 1.3

--- routes/__init__.py
+++ routes/__init__.py
@@ -8,7 +8,7 @@ def homepage():
     request_context = {"1": "ABC"}
     if request.method == 'POST':
         input_string = request.form.get("user_input")
-        flash(input_string)
+        flash(input_string + ", Please hire me!")
         # return render_template("base.jinja2", **request_context)
     return render_template("base.jinja2", **request_context)
 
